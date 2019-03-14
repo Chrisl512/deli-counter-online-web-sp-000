a78e64918c09b7b985fb4996d92f952aaf4e4bb4
Christopher Luna <chris@illusionlayervfx.com>
2019-03-14 18:17:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(array)
         people << nam
         end
 
-    puts "The line is currently: #{position.zip(people).flatten}"
+    puts "The line is currently: #{position.zip(people).flatten.compact}"
     end
 
 end
