fa8af3e681275f77ad1f3018d06c877bfce937df
Christopher Luna <chris@illusionlayervfx.com>
2019-03-14 17:48:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(array)
         people << nam
         end
 
-    puts "The line is currently: #{position.join(array)}  #{people.join}"
+    puts "The line is currently: #{position.join(array)}"
     end
 
 end
