ca9b59329b9421af674e94deac0859246e8adc13
Christopher Luna <chris@illusionlayervfx.com>
2019-03-14 16:44:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  position = []
+  people = []
+    if array == []
+      puts "The line is currently empty."
+    else
+        array.collect!.with_index do |person, index|
+        num = person.to_i + 1
+        position << num
+        nam = person
+        people << nam
+        end
+
+    end
+    puts "The line is currently: #{position} + #{people}"
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{} in line."
+end
+
+def now_serving()
+  puts "There is nobody waiting to be served!"
+end
